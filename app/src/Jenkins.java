51171ef1222cd41a96c16a5f1fe269a68ed8fc63
ADiTY0102 <adityabinjagermath12@gmail.com>
2025-10-26 15:37:21 +0530

    10/26/2025 3:18 pm

---
+++ app/src/Jenkins.java
@@ -0,0 +1,5 @@
+public class Jenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!"+" Updated on 10/26/2025 3:18 pm");
+    }
+}
--- app/src/JenkinsApp.java
+++
@@ -1,6 +0,0 @@
-public class JenkinsApp {
-    public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
-    }
-
-}
